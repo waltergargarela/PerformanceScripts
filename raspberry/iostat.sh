a9ae6d46d50149d2aea612b5b9f858561f079b98
Walter Gargarela Junior <walter.gargarela@gmail.com>
2015-03-27 16:43:10 -0300

    iostat script to use with raspberry pi

---
+++ raspberry/iostat.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+## Script para monitoramento de sistema com o cmd "iostat" ##
+#
+#name: iostat.ksh
+#date: 05/01/05
+# Walter Gargarela Junior / Ismar Rainer de Castro
+
+
+## Variaveis do iostat.ksh ##
+
+bold=`tput smso;tput bold`
+offbold=`tput rmso`
+hostname=`uname -n`
+PS3="ENTRE COM A OPCAO DESEJADA: "
+
+## Variaveis "iostat" Screen_Mode ##
+
+TTY=`tty |sed "s:\/dev\/::g"`
+ID_DISP=`who |grep pts/12|nawk '{print $6}'|sed "s:(::g"|sed "s:)::g"`
+DISPLAY=localhost$ID_DISP
+
+export DISPLAY
+
+##Limpa Tela 
+
+clear
+
+## Tela Principal ##
+
+echo -e "\n ${bold} *** IOSTAT.SH *** ${offbold} " 
+echo -e "\n hostname => ${bold} $hostname ${offbold} "
+#print "system details =>\n $detail "
+echo  -e "\n #################################### \n"
+
+select applic in Screen_Mode Report_Mode Man_Iostat Sair
+do 
+	case $applic in
+		Screen_Mode)
+			echo -e "\n Comando sugerido iostat -h 1\n " 
+			read -p "Insira a bagaca: " opcao
+			read -p "Insira o tempo de intervalo: " intervalo
+			read -p "Insira a quantidade de intervalos: " quant
+			/usr/bin/iostat $opcao $intervalo $quant
+			sleep 5
+			break # pkill iostat.ksh
+			clear 
+			;;
+
+		Report_Mode)
+			read -p "insira a opcao do iostat: " opcao
+			read -p "insira o tempo de intervalo: " intervalo
+                        read -p "insira a quantidade de intervalos: " quant
+			read -p "indique o local e nome do arquivo: " loc_file
+                        /usr/bin/iostat $opcao $intervalo $quant  > $loc_file &
+			echo -e "\n salvando arquivo >>$loc_file<< "
+                        sleep 5
+                        ./iostat.sh
+                        break # pkill iostat.ksh
+                        clear
+                        ;;
+
+		Man_Iostat)
+			/usr/bin/man iostat 
+			./iostat.sh
+			break # pkill iostat.ksh
+			clear
+			;;
+
+		Sair)
+			break
+			;;
+		*)
+			echo "Entre com uma opcao valida."
+	                ;;
+        esac
+done
+clear
+
